e1d39844f65eaf50190c06813d1775ad138627e6
baby李 <2105451348@qq.com>
2022-09-08 11:08:28 +0800

    feat: mention finished

--- src/events/messageEvent.ts
+++ src/events/messageEvent.ts
@@ -1,4 +1,5 @@
 import { request } from '../utils/request'
+import { userMap } from '../data/userList'
 import type { baseEvent, messageD } from '../types/event'
 
 interface wordsParam {
@@ -8,8 +9,19 @@ interface wordsParam {
 }
 
 export const wordsEvent = (param: wordsParam) => {
+  // 检查是否有提到用户
+  let mentionArr = param.messageBody.d.extra.mention
+  let cq_code = ''
+  if (mentionArr.length !== 0) {
+    for (let i = 0; i < mentionArr.length; i++) {
+      cq_code += `[CQ:at,qq=${userMap.get(mentionArr[i])}] `
+    }
+  }
+
   request.post(`${param.qq_http}/send_group_msg`, {
     group_id: param.group_id,
-    message: `[来自 KooK] ${param.messageBody.d.extra.author.nickname}: ${param.messageBody.d.extra.kmarkdown.raw_content}`
+    message: `[来自 KooK] ${param.messageBody.d.extra.author.nickname}: ${
+      param.messageBody.d.extra.kmarkdown.raw_content
+    } ${mentionArr.length !== 0 ? cq_code : ''}`
   })
 }
